ded116b82ae6ad3a1f5dc2af5b5f5dd470ef346b
Himanshu4782652 <himanshusriwastava111@gmail.com>
2024-12-22 20:41:02 +0530

    adding model in admin model

--- AdminPanel/app/admin.py
+++ AdminPanel/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import * #now, we can access all models that was created in models.py
 
 # Register your models here.
+admin.site.register(Teacher)
--- AdminPanel/app/models.py
+++ AdminPanel/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#Model created
+class Teacher(models.Model):
+ FirstName=models.CharField(max_length=50)
+ LastName=models.CharField(max_length=50)
+ Email=models.EmailField(max_length=50)
+ Contact=models.CharField(max_length=50)
+ 
+ #this function is used for converting object into strings
+ def __str__(self) -> str:
+  return self.FirstName

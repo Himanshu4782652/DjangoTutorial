466f6c1505c75019aea8b50f9e5bfcdb14290e64
Himanshu4782652 <himanshusriwastava111@gmail.com>
2024-12-27 23:22:45 +0530

    user registration with server-side validation

--- Registation_Template_Integration/app/views.py
+++ Registation_Template_Integration/app/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 #view for register page
 def RegisterPage(request):
  return render(request,"app/register.html")
+
+#view for user registration
+def UserRegister(request):
+ if request.method=="POST":
+  fname=request.POST['fname']
+  lname=request.POST['lname']
+  email=request.POST['email']
+  contact=request.POST['contact']
+  password=request.POST['password']
+  cpassword=request.POST['cpassword']
+  #first we will validate that the user already exist
+  user=User.objects.filter(Email=email)
+  if user:
+   message="user already exist"
+   return render(request,"app/register.html",{'msg':message})
+  else:
+   if password==cpassword:
+    newuser=User.objects.create(Firstname=fname,Lastname=lname,Email=email,
+                                Contact=contact,Password=password)
+    message="User register successfully"
+    return render(request,"app/login.html",{'msg':message})
+   else:
+    message="Password and ConformPassword does not match"
+    return render(request,"app/register.html",{'msg':message})
--- Registation_Template_Integration/project/settings.py
+++ Registation_Template_Integration/project/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "registerUser",
+        "USER": "himanshu",
+        "PASSWORD": "Himanshu@0519",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 

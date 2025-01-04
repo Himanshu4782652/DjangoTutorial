b6293e9a0e22951587650e1af69a212db01a9344
Himanshu4782652 <himanshusriwastava111@gmail.com>
2025-01-05 02:47:04 +0530

    project job portal

--- project/jobPortal/app/urls.py
+++ project/jobPortal/app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
   path("",views.IndexPage,name="index"), 
+  path("signup/",views.SignupPage,name="signup"),
+  path("register/",views.RegisterUser,name="register"),
+  path("registercompany/",views.RegisterCompany,name="registercompany"),
+  path("otppage/",views.OTPPage,name="otppage"),
+  path("otp/",views.Otpverify,name="otp"),
 ]
--- project/jobPortal/app/views.py
+++ project/jobPortal/app/views.py
@@ -1,6 +1,83 @@
 from django.shortcuts import render
+from .models import *
+from random import randint
 
 # Create your views here.
 
+
 def IndexPage(request):
- return render(request,"app/index.html")
+   return render(request, "app/index.html")
+
+
+def SignupPage(request):
+   return render(request, "app/signup.html")
+
+
+def RegisterUser(request):
+   if request.POST["role"] == "Candidate":
+      role = request.POST["role"]
+      fname = request.POST["firstname"]
+      lname = request.POST["lastname"]
+      email = request.POST["email"]
+      password = request.POST["password"]
+      cpassword=request.POST["cpassword"]
+
+      # server-side validation
+      user = UserMaster.objects.filter(email=email)
+      if user:
+         message = "User already exist"
+         return render(request, "app/signup.html", {"msg": message})
+      else:
+         if password == cpassword:
+            otp = randint(100000, 999999)
+            newuser = UserMaster.objects.create(
+               role=role, otp=otp, email=email, password=password
+               )
+            newcand = Candidate.objects.create(
+               user_id=newuser, firstname=fname, lastname=lname
+               )
+            return render(request, "app/otpverify.html",{'email':email})
+   else:
+      print("Company registration")
+
+def RegisterCompany(request):
+   if request.POST["role"]=="Company":
+      role=request.POST["role"]
+      fname=request.POST["firstname"]
+      lname = request.POST["lastname"]
+      email = request.POST["email"]
+      password = request.POST["password"]
+      cpassword=request.POST["cpassword"]
+      
+      #server side validation
+      user = UserMaster.objects.filter(email=email)
+      if user:
+         message = "Company already exist"
+         return render(request, "app/signup.html", {"msg": message})
+      else:
+            if password == cpassword:
+               otp = randint(100000, 999999)
+               newuser = UserMaster.objects.create(
+               role=role, otp=otp, email=email, password=password)
+               newcomp = Company.objects.create(user_id=newuser, firstname=fname, lastname=lname)
+               return render(request, "app/otpverify.html",{'email':email})
+            else:
+               print("Candidate registration")
+         
+def OTPPage(request):
+   return render(request,"app/otpverify.html")
+
+def Otpverify(request):
+   email=request.POST['email']
+   otp=int(request.POST['otp'])
+   
+   user=UserMaster.objects.get(email=email)
+   if user:
+      if user.otp==otp:
+         message="Otp Verification Successful"
+         return render(request,"app/login.html", {'msg':message})
+      else:
+         message="Otp is incorrect"
+         return render(request,"app/otpverify.html",{'msg':message})
+   else:
+      return render(request,"app/signup.html")

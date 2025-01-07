750cec008333c0a4cedf912808215c5d196158af
Himanshu4782652 <himanshusriwastava111@gmail.com>
2025-01-07 23:06:16 +0530

    minor fixes

--- project/jobPortal/app/views.py
+++ project/jobPortal/app/views.py
@@ -38,32 +38,24 @@ def RegisterUser(request):
                )
             return render(request, "app/otpverify.html",{'email':email})
    else:
-      print("Company registration")
-
-# def RegisterCompany(request):
-#    if request.POST["role"]=="Company":
-#       role=request.POST["role"]
-#       fname=request.POST["firstname"]
-#       lname = request.POST["lastname"]
-#       email = request.POST["email"]
-#       password = request.POST["password"]
-#       cpassword=request.POST["cpassword"]
-      
-#       #server side validation
-#       user = UserMaster.objects.filter(email=email)
-#       if user:
-#          message = "Company already exist"
-#          return render(request, "app/signup.html", {"msg": message})
-#       else:
-#             if password == cpassword:
-#                otp = randint(100000, 999999)
-#                newuser = UserMaster.objects.create(
-#                role=role, otp=otp, email=email, password=password)
-#                newcomp = Company.objects.create(user_id=newuser, firstname=fname, lastname=lname)
-#                return render(request, "app/otpverify.html",{'email':email})
-#             else:
-#                print("Candidate registration")
-
+      if request.POST["role"]=="Company":
+         role=request.POST['role']
+         fname=request.POST['firstname']
+         lname=request.POST['lastname']
+         email=request.POST['email']
+         password=request.POST['password']
+         cpassword=request.POST['cpassword']
+         
+         user1=UserMaster.objects.filter(email=email)
+         if user1:
+            message="Company already exist"
+            return render(request, "app/signup.html", {"msg":message})
+         else:
+            if password==cpassword:
+               otp=randint(100000,999999)
+               newuser1=UserMaster.objects.create(role=role,otp=otp,email=email,password=password)
+               newcomp=Company.objects.create(user_id=newuser1,firstname=fname,lastname=lname)
+               return render(request, "app/otpverify.html",{'email':email})
 
 def OTPPage(request):
    return render(request,"app/otpverify.html")
